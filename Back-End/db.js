829caae316fdfdb237546554629cc0adb972a9ad
AFZAL HASSAN <105422150+ARIYANTABISH@users.noreply.github.com>
2022-10-15 01:14:42 +0500

    Update db.js

--- Back-End/db.js
+++ Back-End/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb://localhost:27017/inotebook?........................."
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
